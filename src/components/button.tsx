f4e93e69fd125e015d7eab307520659740379b00
2305ray <rayssagarcia035@gmail.com>
2025-02-20 13:18:59 -0300

    input e biome

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,16 +1,14 @@
-import { ComponentProps, ReactNode } from "react"
+import { type ComponentProps, ReactNode } from 'react'
 
 interface ButtoProps extends ComponentProps<'button'> {
-// children: ReactNode, não é necessario, pq o button ja espera q passe algo dentro dele
+  // children: ReactNode, não é necessario, pq o button ja espera q passe algo dentro dele
 }
 
-export function Button(props :ButtoProps) {
-    return (
-
-    <button 
-    className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full  cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-700"
-    {...props} // pega todas as props q tem e envia aí dentro, ou seja, o children ja vai
-     />
-       
+export function Button(props: ButtoProps) {
+  return (
+    <button
+      className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full  cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-700"
+      {...props} // pega todas as props q tem e envia aí dentro, ou seja, o children ja vai
+    />
   )
-} 
+}
--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,19 +1,16 @@
-import { ComponentProps, ReactNode } from "react"
+import { type ComponentProps, ReactNode } from 'react'
 
 interface IconButtoProps extends ComponentProps<'button'> {
-// children: ReactNode, não é necessario, pq o button ja espera q passe algo dentro dele
+  // children: ReactNode, não é necessario, pq o button ja espera q passe algo dentro dele
 }
 
-
-export function IconButton(props :IconButtoProps) {
-    return (
-
-    <button 
-    className="p-1.5 bg-gray-500 text-blue rounded-md   cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-700" 
-    {...props}
+export function IconButton(props: IconButtoProps) {
+  return (
+    <button
+      className="p-1.5 bg-gray-500 text-blue rounded-md   cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-700"
+      {...props}
     />
     //     {props.children}
     // </button>
-  
   )
-} 
+}
